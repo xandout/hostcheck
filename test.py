d927bf3b532416b6d50c6fe399ad44300301cd3c
Mitchell Turner <turnerdistributed@gmail.com>
2013-06-30 23:45:12 -0400

    Looking at different ways to establish connection

--- test.py
+++ test.py
@@ -1,9 +1,23 @@
 #!/usr/bin/python
 __author__ = 'Mitchell'
 import socket
-sock = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+from threading import Thread
+IPs = '192.168.112.'
 
-try:
-    sock.connect(('google.com', 80))
-except:
-    print("FAIL")
+
+
+
+
+for ip in range (60,86):
+    try:
+        sock = socket.socket(socket.AF_INET , socket.SOCK_STREAM)
+        sock.setblocking(0)
+        test = IPs + str(ip)
+
+        sock.connect((test, 3389))
+        print(IPs + str(ip) + ' NCJBOD' + str(ip-50))
+        sock.close()
+    except socket.error as msg:
+        print(IPs + str(ip), msg)
+        sock.close()
+
